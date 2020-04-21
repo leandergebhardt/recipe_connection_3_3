68701a24b7714caaa8d68f4f2bfd0939da0f7585
Leander Gebhardt <s0565964@htw-berlin.de>
2020-04-21 19:09:17 +0200

    added messages module

---
+++ modules/messages.js
@@ -0,0 +1,1 @@
+exports.messages = ['You are beautiful!', 'I am glad you are here!', 'I love this'];
---
+++ printMessages.js
@@ -0,0 +1,4 @@
+const messagesModule = require("./modules/messages");
+messagesModule.messages.forEach(m => {
+  console.log(m);
+});
